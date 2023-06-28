0425ee76724dbb7d68e802373b322d5d09fb759b
Volodya9 <vladadespenic90@gmail.com>
2023-06-28 16:38:15 +0200

    Added new MVC project for StudentApp

---
+++ MVCdemo/MVCdemo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            string msg = "Hello world";
+            return msg;
+        }
+    }
+}
