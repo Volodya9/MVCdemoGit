125ba45f796a01977e4ad1a4467d1aaee1ff4a22
Volodya9 <81824327+Volodya9@users.noreply.github.com>
2024-02-23 16:30:44 +0100

    Update HomeController.cs removed Index action

--- MVCdemo/MVCdemo/Controllers/HomeController.cs
+++ MVCdemo/MVCdemo/Controllers/HomeController.cs
@@ -8,12 +8,7 @@ namespace MVCdemo.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public string Index()
-        {
-            string msg = "Hello world";
-            return msg;
-        }
+        
 
         public string Demo()
         {
